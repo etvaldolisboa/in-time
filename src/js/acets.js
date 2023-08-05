63d45e212196a04e7339bc1c270de87494205eaa
Etvaldo Lisboa <etvaldolisboa@hotmail.com>
2023-08-05 00:55:57 -0400

    ajust no layout

--- src/js/acets.js
+++ src/js/acets.js
@@ -1,6 +1,3 @@
-const anos = document.getElementById('ano');
-const meses = document.getElementById('mes');
-const dias = document.getElementById('dia');
 const horas = document.getElementById('hora');
 const minutos = document.getElementById('minuto');
 const segundos = document.getElementById('segundo');
@@ -8,23 +5,14 @@ const segundos = document.getElementById('segundo');
 const relogio = setInterval(function time() {
     let dateToday = new Date();
 
-    let ano = dateToday.getFullYear();
-    let mes = dateToday.getMonth();
-    let dia = dateToday.getDate();
     let hr = dateToday.getHours();
     let min = dateToday.getMinutes();
     let seg = dateToday.getSeconds();
 
-    if (ano < 1000) ano = '0' + ano;
-    if (mes < 10) mes = '0' + mes;
-    if (dia < 10) dia = '0' + dia;
     if (hr < 10) hr = '0' + hr;
     if (min < 10) min = '0' + min;
     if (seg < 10) seg = '0' + seg;
 
-    anos.textContent = ano;
-    meses.textContent = mes;
-    dias.textContent = dia;
     horas.textContent = hr;
     minutos.textContent = min;
     segundos.textContent = seg;
